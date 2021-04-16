3747cb39e7785867f183b55bb824bd5f69f72c1b
zzupd <zzupd@naver.com>
2021-04-16 12:08:18 +0900

    0416_아이디중복검사_jQuery기능구현완료

--- script/script.js
+++ script/script.js
@@ -1,5 +1,24 @@
 $(function(){
 
+    $("button[type=button]").click(function(){
 
+        var inputUID = $("#uID").val().trim();   // form화면에서 입력한 ID
+        var regIDList = ["test01", "guest", "member03"];  // 가입된 ID를 의미
+
+        var idUseChk = true;
+
+        $.each(regIDList, function(i, v){
+            if(inputUID == v) {
+                idUseChk = false;
+            }
+        });
+
+        if(idUseChk) {
+            alert("사용가능");
+        } else {
+            alert("사용중인 ID");
+        }
+
+    });
 
 });
