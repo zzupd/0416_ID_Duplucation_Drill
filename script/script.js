39e818bfabbc70671f2aaef52bc0a15fca3fefa1
zzupd <zzupd@naver.com>
2021-04-16 15:55:25 +0900

    0416_비밀번호보기구현

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    /* 아이디 중복 체크 시작 */
     $("button[type=button]").click(function(){
 
         var inputUID = $("#uID").val().trim();   // form화면에서 입력한 ID
@@ -31,11 +32,26 @@ $(function(){
 
 
     });
+    /* 아이디 중복 체크 끝 */
 
+
+    /* 입력취소 클릭이벤트 아이디 중복 메시지 제거 */
     $("button[type=reset]").click(function(){
         $("span#idChkMsg").remove();
     });
+    /* 입력취소 클릭이벤트 아이디 중복 메시지 제거 */
 
+
+    /* 비밀번호 보기 */
+    $("input[type=checkbox]").click(function(){
+        var chkTF = $(this).prop("checked");
+        if (chkTF) {
+            $("input.uPW").attr("type", "text");
+        } else {
+            $("input.uPW").attr("type", "password");
+        }
+    });
+    /* 비밀번호 보기 */
 
 
 
