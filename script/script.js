94f4d0e173b8f2b01a7d6b03fef86ca7c5c8a08b
zzupd <zzupd@naver.com>
2021-04-16 16:37:44 +0900

    0416_회원가입_비밀번호 동일검사

--- script/script.js
+++ script/script.js
@@ -1,8 +1,9 @@
 $(function(){
 
-    /* 아이디 중복 체크 시작 */
+
     $("button[type=button]").click(function(){
 
+    /* 아이디 중복 체크 시작 */
         var inputUID = $("#uID").val().trim();   // form화면에서 입력한 ID
         var regIDList = ["test01", "guest", "member03"];  // 가입된 ID를 의미
 
@@ -28,11 +29,32 @@ $(function(){
         }
 
         $("<span id='idChkMsg'>"+ msg +"</span>").insertAfter("#uID");
-        // $("A").insertAfter("B);  요소A를 요소B 뒤로 이동하세요
+    /* 아이디 중복 체크 끝 */
+
+
+    /* 비밀번호 일관성(=동일성)검사 일치여부 점검 */
+        var uPW = $("#uPW").val().trim();
+        var uPW_Re = $("#uPW_Re").val().trim();
+
+        var pwChkMsg="";    // message
+        if (uPW == uPW_Re) {
+            pwChkMsg = "비밀번호 일치";
+        } else {
+            pwChkMsg = "불일치";
+        }
+
+        var pwSpanChk = $("#uPW_Re").next().is("span");
+        // span요소 유무 체크
+        if (pwSpanChk) {
+            $("span#pwChkMsg").remove();
+        }
+        $("<span id='pwChkMsg'>"+ pwChkMsg +"</span>").insertAfter("#uPW_Re");
+
+    /* 비밀번호 일관성(=동일성)검사 일치여부 점검 */
+
 
 
     });
-    /* 아이디 중복 체크 끝 */
 
 
     /* 입력취소 클릭이벤트 아이디 중복 메시지 제거 */
@@ -52,6 +74,41 @@ $(function(){
         }
     });
     /* 비밀번호 보기 */
+
+
+
+
+    /* 추가내용 (포커스 아웃 이벤트 적용) */
+    $("#uID").focusout(function(){
+        /* 아이디 중복 체크 시작 */
+        var inputUID = $("#uID").val().trim();   // form화면에서 입력한 ID
+        var regIDList = ["test01", "guest", "member03"];  // 가입된 ID를 의미
+
+        var idUseChk = true;
+
+        $.each(regIDList, function(i, v){
+            if(inputUID == v) {
+                idUseChk = false;
+            }
+        });
+
+        var msg="";    // message
+        if(idUseChk) {
+            msg = "사용가능";
+        } else {
+            msg = "사용중인 ID"
+        }
+
+        var spanChk = $("#uID").next().is("span");
+        // span요소 유무 체크
+        if (spanChk) {
+            $("span#idChkMsg").remove();
+        }
+
+        $("<span id='idChkMsg'>"+ msg +"</span>").insertAfter("#uID");
+    /* 아이디 중복 체크 끝 */
+    });
+    /* 추가내용 (포커스 아웃 이벤트 적용) */
 
 
 
